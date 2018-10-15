2687a5c4ceec5872d6fedfbf28b3c89cc24da78b
Hong Wang <hongwang600@cs.ucsb.edu>
2018-10-14 20:39:13 -0700

    add feed_samples

--- train.py
+++ train.py
@@ -17,6 +17,46 @@ model_path = conf['model_path']
 device = conf['device']
 lr = conf['learning_rate']
 
+def feed_samples(model, samples, loss_function, all_relations, device):
+    questions, relations, relation_set_lengths = process_samples(
+        samples, all_relations, device)
+    #print('got data')
+    ranked_questions, reverse_question_indexs = \
+        ranking_sequence(questions)
+    ranked_relations, reverse_relation_indexs =\
+        ranking_sequence(relations)
+    question_lengths = [len(question) for question in ranked_questions]
+    relation_lengths = [len(relation) for relation in ranked_relations]
+    #print(ranked_questions)
+    pad_questions = torch.nn.utils.rnn.pad_sequence(ranked_questions)
+    pad_relations = torch.nn.utils.rnn.pad_sequence(ranked_relations)
+    #print(pad_questions)
+    pad_questions = pad_questions.to(device)
+    pad_relations = pad_relations.to(device)
+    #print(pad_questions)
+
+    model.zero_grad()
+    model.init_hidden(device, sum(relation_set_lengths))
+    all_scores = model(pad_questions, pad_relations, device,
+                       reverse_question_indexs, reverse_relation_indexs,
+                       question_lengths, relation_lengths)
+    all_scores = all_scores.to('cpu')
+    pos_scores = []
+    neg_scores = []
+    start_index = 0
+    for length in relation_set_lengths:
+        pos_scores.append(all_scores[start_index].expand(length-1))
+        neg_scores.append(all_scores[start_index+1:start_index+length])
+        start_index += length
+    pos_scores = torch.cat(pos_scores)
+    neg_scores = torch.cat(neg_scores)
+
+    loss = loss_function(pos_scores, neg_scores,
+                         torch.ones(sum(relation_set_lengths)-
+                                    len(relation_set_lengths)))
+    loss.backward()
+
+
 def train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
           device, batch_size, lr, model_path, embedding, all_relations,
           model=None, epoch=100):
@@ -32,43 +72,7 @@ def train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
         #training_data = training_data[0:100]
         for i in range((len(training_data)-1)//batch_size+1):
             samples = training_data[i*batch_size:(i+1)*batch_size]
-            questions, relations, relation_set_lengths = process_samples(
-                samples, all_relations, device)
-            #print('got data')
-            ranked_questions, reverse_question_indexs = \
-                ranking_sequence(questions)
-            ranked_relations, reverse_relation_indexs =\
-                ranking_sequence(relations)
-            question_lengths = [len(question) for question in ranked_questions]
-            relation_lengths = [len(relation) for relation in ranked_relations]
-            #print(ranked_questions)
-            pad_questions = torch.nn.utils.rnn.pad_sequence(ranked_questions)
-            pad_relations = torch.nn.utils.rnn.pad_sequence(ranked_relations)
-            #print(pad_questions)
-            pad_questions = pad_questions.to(device)
-            pad_relations = pad_relations.to(device)
-            #print(pad_questions)
-
-            model.zero_grad()
-            model.init_hidden(device, sum(relation_set_lengths))
-            all_scores = model(pad_questions, pad_relations, device,
-                               reverse_question_indexs, reverse_relation_indexs,
-                               question_lengths, relation_lengths)
-            all_scores = all_scores.to('cpu')
-            pos_scores = []
-            neg_scores = []
-            start_index = 0
-            for length in relation_set_lengths:
-                pos_scores.append(all_scores[start_index].expand(length-1))
-                neg_scores.append(all_scores[start_index+1:start_index+length])
-                start_index += length
-            pos_scores = torch.cat(pos_scores)
-            neg_scores = torch.cat(neg_scores)
-
-            loss = loss_function(pos_scores, neg_scores,
-                                 torch.ones(sum(relation_set_lengths)-
-                                            len(relation_set_lengths)))
-            loss.backward()
+            feed_samples(model, samples, loss_function, all_relations, device)
             optimizer.step()
         acc=evaluate_model(model, valid_data, batch_size, all_relations, device)
         if acc > best_acc:
