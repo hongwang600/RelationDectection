2d4a6c3e6dcc6f660941487adea68946777be57c
Hong Wang <hongwang600@cs.ucsb.edu>
2018-10-23 23:44:02 -0700

    back to whole train same candidate set

--- continue_train.py
+++ continue_train.py
@@ -84,9 +84,10 @@ def run_sequence(training_data, testing_data, valid_data, all_relations,
         for j in range(i+1):
             current_test_data.append(
                 remove_unseen_relation(splited_test_data[j], seen_relations))
-        current_model = None
+        #current_model = None
         #all_seen_data += current_train_data
         all_seen_data += splited_training_data[i]
+        #random.shuffle(all_seen_data)
         current_model = train(all_seen_data, current_valid_data,
                               vocabulary, embedding_dim, hidden_dim,
                               device, batch_size, lr, model_path,
