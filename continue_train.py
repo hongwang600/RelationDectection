710fe29578b793852c7be7a936c66300998447d0
Hong Wang <hongwang600@cs.ucsb.edu>
2018-11-11 00:14:36 -0800

    fisher

--- continue_train.py
+++ continue_train.py
@@ -68,6 +68,82 @@ def get_que_embed(model, sample_list, all_relations):
         ret_que_embeds.append(que_embeds.detach().cpu().numpy())
     return np.concatenate(ret_que_embeds)
 
+def gen_fisher(model, train_data, all_relations):
+    num_correct = 0
+    #testing_data = testing_data[0:100]
+    softmax_func = nn.LogSoftmax(0)
+    loss_func = nn.NLLLoss()
+    fisher_batch_size = 1
+    batch_epoch = (len(train_data)-1)//fisher_batch_size+1
+    fisher = None
+    for i in range(batch_epoch):
+        model.zero_grad()
+        losses = []
+        samples = train_data[i*fisher_batch_size:(i+1)*fisher_batch_size]
+        questions, relations, relation_set_lengths = process_samples(
+            samples, all_relations, device)
+        #print('got data')
+        ranked_questions, reverse_question_indexs = \
+            ranking_sequence(questions)
+        ranked_relations, reverse_relation_indexs =\
+            ranking_sequence(relations)
+        question_lengths = [len(question) for question in ranked_questions]
+        relation_lengths = [len(relation) for relation in ranked_relations]
+        #print(ranked_questions)
+        pad_questions = torch.nn.utils.rnn.pad_sequence(ranked_questions)
+        pad_relations = torch.nn.utils.rnn.pad_sequence(ranked_relations)
+        #print(pad_questions)
+        pad_questions = pad_questions.to(device)
+        pad_relations = pad_relations.to(device)
+        #print(pad_questions)
+
+        model.init_hidden(device, sum(relation_set_lengths))
+        all_scores = model(pad_questions, pad_relations, device,
+                           reverse_question_indexs, reverse_relation_indexs,
+                           question_lengths, relation_lengths)
+        all_scores = all_scores.to('cpu')
+        start_index = 0
+        for length in relation_set_lengths:
+            scores = all_scores[start_index:start_index+length]
+            start_index += length
+            #print(scores)
+            losses.append(loss_func(softmax_func(scores).view(1, -1),
+                                    torch.tensor([0])))
+        loss_batch = sum(losses)
+        #print(loss_batch)
+        loss_batch.backward()
+        grad_params = get_grad_params(model)
+        #for param in grad_params:
+         #   print(param.grad)
+        if fisher is None:
+            fisher = [param.grad**2/batch_epoch
+                         for param in grad_params]
+        else:
+            fisher = [fisher[i]+param.grad**2/batch_epoch
+                         for i,param in enumerate(grad_params)]
+
+    return fisher
+
+def get_mean_fisher(model, train_data, all_relations):
+    grad_params = get_grad_params(model)
+    grad_mean = copy_param_data(grad_params)
+    grad_fisher = gen_fisher(model, train_data, all_relations)
+    return grad_mean, grad_fisher
+
+def update_fisher(model, train_data, all_relations,
+                  past_fisher, num_past_data):
+    cur_mean, cur_fisher = get_mean_fisher(model, train_data, all_relations)
+    num_cur_data = len(train_data)
+    num_total_data = num_past_data + num_cur_data
+    if past_fisher is None:
+        past_fisher = cur_fisher
+    else if cur_fisher is not None:
+        for i in range(len(past_fisher)):
+            #past_fisher[i] = past_fisher[i]*num_past_data/num_total_data +\
+            #    cur_fisher[i]*num_cur_data/num_total_data
+            past_fisher[i] = torch.max(past_fisher[i], cur_fisher[i])
+    return past_fisher, num_total_data
+
 def select_data(model, samples, num_sel_data, all_relations):
     que_embeds = get_que_embed(model, samples, all_relations)
     #print(que_embeds[:5])
@@ -122,6 +198,7 @@ def run_sequence(training_data, testing_data, valid_data, all_relations,
     sequence_results = []
     #np.set_printoptions(precision=3)
     result_whole_test = []
+    past_fisher = None
     for i in range(num_clusters):
         seen_relations += [data[0] for data in splited_training_data[i] if
                           data[0] not in seen_relations]
@@ -137,7 +214,12 @@ def run_sequence(training_data, testing_data, valid_data, all_relations,
                               vocabulary, embedding_dim, hidden_dim,
                               device, batch_size, lr, model_path,
                               embedding, all_relations, current_model, epoch,
-                              memory_data, loss_margin)
+                              memory_data, loss_margin, past_fisher)
+        past_fisher, num_past_data = update_fisher(current_model,
+                                                   current_train_data,
+                                                   all_relations,
+                                                   past_fisher,
+                                                   num_past_data)
         #memory_data.append(current_train_data[-task_memory_size:])
         #memory_data.append(splited_training_data[i][-task_memory_size:])
         memory_data.append(select_data(current_model, current_train_data,
